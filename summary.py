b25a090d8ea561148430ed9c36ac52a7681eacb3
sanikapatil <sanikapatil.work@gmail.com>
2025-09-23 16:09:40 +0530

    adding expense to google sheet- first stage

--- summary.py
+++ summary.py
@@ -10,6 +10,8 @@ from dotenv import load_dotenv
 from typing import TypedDict
 from twilio.rest import Client
 from langgraph.graph import StateGraph,START,END
+import os, requests, gspread
+from google.oauth2.service_account import Credentials
 
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
@@ -67,12 +69,13 @@ _to_wa=os.getenv("TWILIO_WHATSAPP_TO")
 
 _twilio=Client(_account_sid,_auth_token)
 
-
+# fuction to send actual summary to WB
 def send_whatsapp_text(body:str,to:str=None):
     "send the Whatsapp message via Twilio"
     msg=_twilio.messages.create(from_=_from_wa,to=_to_wa,body=body)
     print("Twilio SID:",msg.sid)
 
+# node for send summary to whatsapp 
 def send_to_whatsapp_node(state:ExpenseState):
     "LangGraph node:to send whatsapp messages"
     # print("###"*12)
@@ -80,6 +83,7 @@ def send_to_whatsapp_node(state:ExpenseState):
     send_whatsapp_text(state["summary"])
     return {}
 
+# node to generate summary from json data using LLM
 def summarize_expenses(state:ExpenseState):
     "lanngraph node to summarize_expenses"
     payload=state["data_json"]
@@ -88,8 +92,15 @@ def summarize_expenses(state:ExpenseState):
     (
         "system",
         "You are a helpful assistant that creates clear and concise daily expense summaries. "
-        "Always group expenses by category, show totals, and provide a short overall summary. "
-        "Keep the tone simple and friendly, suitable for sending as a WhatsApp message."
+         "Your tasks:\n"
+        "1. Group expenses by category.\n"
+        "2. Show totals per category and the grand total.\n"
+        "3. Provide a short, friendly summary description.\n"
+        "4. Use simple, WhatsApp-friendly language with emojis.\n"
+        "5. Do NOT use symbols like *, #, $, or bullet points. "
+        "Instead, keep formatting natural (plain text + emojis).\n"
+        "6. Make it short and easy to read in a single WhatsApp message."
+        "7.also provide a short description of each expense"
     ),
     (
 
@@ -103,6 +114,33 @@ def summarize_expenses(state:ExpenseState):
     return {'summary':result.content}
 
 
+def log_to_news_google_sheet(state:ExpenseState):
+    """Log message to Google Sheet"""
+    # Google Sheets setup
+    SCOPE = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPE)
+    client = gspread.authorize(creds)
+    sheet_id = os.getenv("SHEET_ID") # os.getenv("SHEET_NAME")
+    workbook = client.open_by_key(sheet_id)  # First sheet
+    users_sheet = workbook.worksheet("sept_sheet")
+
+      # ✅ Ensure headers exist
+    headers = users_sheet.row_values(1)  # first row
+    if headers != ["Date", "Expense_summary"]:
+        users_sheet.update("A1:B1", [["Date", "Expense_summary"]])
+    
+    users_sheet.append_row([state['date'],state['summary']])
+
+    return {}
+
+    # Current date
+    # date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Each row will be: [Date, Message]
+    # for msg in messages:
+    #     users_sheet.append_row([date_str, msg])
+
+
 
 graph=StateGraph(ExpenseState)
 
@@ -110,12 +148,14 @@ graph=StateGraph(ExpenseState)
 graph.add_node('expense_by_date',expense_by_date)
 graph.add_node('summarize_expenses',summarize_expenses)
 graph.add_node('send_to_whatsapp_node',send_to_whatsapp_node)
+graph.add_node('log_to_news_google_sheet',log_to_news_google_sheet)
 
 # add Edges
 graph.add_edge(START,"expense_by_date")
 graph.add_edge('expense_by_date','summarize_expenses')
 graph.add_edge('summarize_expenses','send_to_whatsapp_node')
-graph.add_edge('send_to_whatsapp_node',END)
+graph.add_edge('send_to_whatsapp_node','log_to_news_google_sheet')
+graph.add_edge('log_to_news_google_sheet',END)
 
 # compile
 workflow=graph.compile()
