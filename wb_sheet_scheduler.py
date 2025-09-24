8b7ce6245223b9f8faf2c0a327f3c9851fb544ce
sanikapatil <sanikapatil.work@gmail.com>
2025-09-24 11:49:52 +0530

    scheduled time to run wb_sheet_scheduler file automatically

--- wb_sheet_scheduler.py
+++ wb_sheet_scheduler.py
@@ -4,44 +4,27 @@ import time
 from summary import * # import your workflow from summary.py
 
 def task1():
-    print("📌 Running daily expense workflow...")
-    # today_date = date.today().strftime("%Y-%m-%d")
-    # try:
-    #     print(type(today_date))
-    #     final_state = workflow.invoke({
-    #     'date':today_date
-    #     })
-    #     # final_state = workflow.invoke({})
-    #     print("✅ Workflow finished:", final_state)
-    # except Exception as e:
-    #     print("❌ Error while running workflow:", e)
+   
+    try:
+        print("📌 Running daily expense workflow...")
+        today_date = date.today().strftime("%Y-%m-%d")
+        print(type(today_date))
+        final_state = workflow.invoke({
+        'date':today_date
+        })
 
-
-    today_date = date.today().strftime("%Y-%m-%d")
-    print(type(today_date))
-    final_state = workflow.invoke({
-    'date':today_date
-    })
-
-    print("Final State:", final_state)
-    print("Daily expense summary sent to WhatsApp ✅")
+        print("Final State:", final_state)
+        print("Daily expense summary sent to WhatsApp ✅")
+    except Exception as e:
+        print("❌ Error while running workflow:", e)
 
 task1()
+# Run every day at 11:00 PM
+schedule.every().day.at("23:01").do(task1)
+schedule.every().day.at("23:00").do(task1)
 
 
- # today_date = date.today().strftime("%Y-%m-%d")
-    # # print(type(today_date))
-    # final_state = workflow.invoke({
-    # 'date':today_date
-    # })
-
-    # print("Final State:", final_state)
-    # print("Daily expense summary sent to WhatsApp ✅")
-
-# Run every day at 9:00 AM
-# schedule.every().day.at("09:00").do(task1)
-
-# print("⏳ Scheduler started... Waiting for 09:00 AM every day.")
-# while True:  #it's like staying awake to check the clock.
-#     schedule.run_pending()   
-#     time.sleep(60)
+print("⏳ Scheduler started... Waiting for 11:00 PM every day.")
+while True:  #it's like staying awake to check the clock.
+    schedule.run_pending()   
+    time.sleep(60)
